58983113afdccec0c51cc36696e8f3fa2cde643a
Alex_Bir <022fincher1989@gmail.com>
2019-04-09 17:29:07 +1000

    маленькая опитимизация

--- src/main/java/UsersRunner.java
+++ src/main/java/UsersRunner.java
@@ -12,6 +12,18 @@ import java.util.List;
 public class UsersRunner {
 
     private static SessionFactory sessionFactory;
+    private static Transaction transaction = null ;
+    private static Session session ;
+
+    public void openTransact(){
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+    }
+
+    public void closeTransact(){
+        transaction.commit();
+        session.close();
+    }
 
     public static void main(String[] args) {
         sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -29,70 +41,67 @@ public class UsersRunner {
 
         System.out.println("___________________________");
         System.out.println("изменю запись 2");
-
         usersRunner.updateUser(2," Вася", " Вася123");
-
         usersRunner.listUsers();
         System.out.println("___________________________");
         System.out.println();
 
         System.out.println("___________________________");
         System.out.println("удалю первую запись");
-
         usersRunner.removeUser(1);
-
         usersRunner.listUsers();
         System.out.println("___________________________");
         System.out.println();
 
+        System.out.println("___________________________");
+        System.out.println("удалю все остальные");
+        usersRunner.removeUser(2);
+        usersRunner.removeUser(3);
+        usersRunner.listUsers();
+        System.out.println("___________________________");
+        System.out.println();
         sessionFactory.close();
     }
 
+
     public void addUser(int id, String userName, String userPass) {
-        Transaction transaction = null ;
-        Session session = sessionFactory.openSession();
-        transaction = session.beginTransaction();
+        openTransact();
         User us = new User(id, userName, userPass);
         session.save(us);
-        transaction.commit();
-        session.close();
+        closeTransact();
     }
 
     public void listUsers() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = null ;
-        transaction = session.beginTransaction();
-        List users = session.createQuery("FROM User").list();
+        openTransact();
 
+        List users = session.createQuery("FROM User").list();
         System.out.println("-----------------------");
         System.out.println("| id| name | password |");
+        System.out.println("-----------------------");
         for (int i = 0 ; i < users.size(); i++)
             System.out.println(users.get(i));
         System.out.println("-----------------------");
 
-        session.close();
-      //  TestTransac.testTransac("вывел на экран", session, transaction);
+        closeTransact();
     }
 
     public  void updateUser(int userId, String userName, String userPasw) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = null ;
-        transaction = session.beginTransaction();
+        openTransact();
+
         User user = (User) session.get(User.class, userId);
         user.setUserName(userName);
         user.setUserPasw(userPasw);
         session.update(user);
-        transaction.commit();
-        session.close();
+
+        closeTransact();
     }
 
     public  void removeUser(int userId){
-        Session sess = sessionFactory.openSession();
-        Transaction trans = null ;
-        trans = sess.beginTransaction();
-        User user = (User) sess.get(User.class, userId);
-        sess.delete(user);
-        trans.commit();
-        sess.close();
+        openTransact();
+
+        User user = (User) session.get(User.class, userId);
+        session.delete(user);
+
+        closeTransact();
     }
 }
