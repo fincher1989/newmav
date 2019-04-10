a31de95a8955bde7b49c051aa8a0de384b876d31
Alex_Bir <022fincher1989@gmail.com>
2019-04-10 18:03:13 +1000

    изменил userService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,45 @@
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         Session session = (new Configuration()).configure().buildSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
+        List users ;
 
-        UsersRunner usersRunner = new UsersRunner();
+        UserService userService = new UserService(session);
 
         System.out.println("___________________________");
         System.out.println("Добавление записей 3 записи");
-        usersRunner.addUser(session,1,"vanya", "vanya123");
-        usersRunner.addUser(session,2,"vasya", "vasya123");
-        usersRunner.addUser(session,3,"petya", "petya123");
-        usersRunner.listUsers(session);
+        userService.addUser(1,"vanya", "vanya123");
+        userService.addUser(2,"vasya", "vasya123");
+        userService.addUser(3,"petya", "petya123");
+        userService.printListUser();
         System.out.println("___________________________");
         System.out.println();
 
         System.out.println("___________________________");
         System.out.println("изменю запись 2");
-        usersRunner.updateUser(session,2," Вася", " Вася123");
-        usersRunner.listUsers(session);
+        userService.updateUser(2," Вася", " Вася123");
+        userService.printListUser();
         System.out.println("___________________________");
         System.out.println();
 
         System.out.println("___________________________");
         System.out.println("удалю первую запись");
-        usersRunner.removeUser(session,1);
-        usersRunner.listUsers(session);
+        userService.removeUser(1);
+        userService.printListUser();
         System.out.println("___________________________");
         System.out.println();
 
         System.out.println("___________________________");
         System.out.println("удалю все остальные");
-        usersRunner.removeUser(session,2);
-        usersRunner.removeUser(session,3);
-        usersRunner.listUsers(session);
+        userService.removeUser(2);
+        userService.removeUser(3);
+        userService.printListUser();
         System.out.println("___________________________");
         System.out.println();
 
---
+++ src/main/java/UserService.java
@@ -0,0 +1,43 @@
+
+import Entity.User;
+
+import org.hibernate.Session;
+
+import java.util.List;
+
+
+public class UserService {
+
+    private Session session ;
+
+    public UserService(Session session) {
+        this.session = session;
+    }
+
+    public void addUser(int id, String userName, String userPass) {
+        User us = new User(id, userName, userPass);
+        session.save(us);
+    }
+
+    public List listUsers() {
+        return session.createQuery("FROM User").list();
+    }
+
+    public void printListUser(){
+        List users = listUsers();
+        for (int i=0; i < users.size(); i++)
+            System.out.println(users.get(i));
+    }
+
+    public  void updateUser(int userId, String userName, String userPasw) {
+        User user = session.get(User.class, userId);
+        user.setUserName(userName);
+        user.setUserPasw(userPasw);
+        session.update(user);
+    }
+
+    public  void removeUser(int userId){
+        User user = session.get(User.class, userId);
+        session.delete(user);
+    }
+}
